0e76935a0d6415dcbdf02e12d41b2a1333f2bd12
TheSuperfon <astefanadon@gmail.com>
2024-11-15 12:01:27 -0500

    Movement Added Task 1
    Movement added (may be edited and improved down the line but it is there now)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,12 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5;
+    Rigidbody2D Rb;
+    public float TimeDelay;
+    public float TimeGone;
     public enum FacingDirection
     {
         left, right
@@ -10,7 +15,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -20,12 +25,45 @@ public class PlayerController : MonoBehaviour
         // then passed in the to the MovementUpdate which should
         // manage the actual movement of the character.
         Vector2 playerInput = new Vector2();
-        MovementUpdate(playerInput);
+        
+
+        playerInput.x = (Input.GetAxisRaw("Horizontal") * speed);
+
+        if (playerInput.x > 0) //when horizontal movement isn't 0 so actually moving
+        {
+            if (TimeGone <= TimeDelay)
+            {
+                TimeGone += Time.deltaTime;
+                Debug.Log(TimeGone);
+            }
+            else
+            {
+                MovementUpdate(playerInput);
+            }
+        }
+        else if (playerInput.x < 0) 
+        {
+            if (TimeGone <= TimeDelay)
+            {
+                TimeGone += Time.deltaTime;
+                Debug.Log(TimeGone);
+            }
+            else
+            {
+                MovementUpdate(playerInput);
+            }
+        }
+        else
+        {
+            TimeGone = 0;
+        }
+
+        
     }
 
     private void MovementUpdate(Vector2 playerInput)
     {
-
+        Rb.velocity = playerInput;
     }
 
     public bool IsWalking()
