a18ac38deed7a9854db8883ecb66b532f4585634
TheSuperfon <astefanadon@gmail.com>
2024-12-02 22:49:39 -0500

    Week12ClassWork
    Just commititng the stuff done in the week 12 class

--- Assets/Scripts/PlayerControl2.cs
+++ Assets/Scripts/PlayerControl2.cs
@@ -14,41 +14,107 @@ public class PlayerControl2 : MonoBehaviour
     {
         left, right
     }
-
+    public enum PlayerState
+    {
+        idle, walking, jumping, dead
+    }
 
 
     private FacingDirection currentDirection = FacingDirection.right;
-    
+
+    public PlayerState currentState = PlayerState.idle;
+    public PlayerState previousState = PlayerState.idle;
+
+    [Header("Horizontal")]
     public float maxSpeed = 5f;
     public float AccelerationTime = 0.25f;
     public float decelerationTime = 0.15f;
+
+    [Header("vertical")]
+    public float apexHeight = 3f;
+    public float apexTime = 0.5f;
+    private float gravity;
+    private float initialJumpSpeed;
+
+
+    [Header("Ground Checking")]
+    public float groundCheckOffset = 0.5f;
+    public Vector2 groundCheckSize = new(0.4f, 0.1f);
+    public LayerMask groundCheckMask;
 
     private Vector2 velocity;
 
     private float accelerationRate;
     private float decelerationRate;
 
-
+    private bool Isgrounded = false;
+    private bool isDead = false;
 
     public void Start()
     {
         body.gravityScale = 0;
         accelerationRate = maxSpeed / AccelerationTime;
         decelerationRate = maxSpeed / decelerationTime;
+
+        gravity = -2 * apexHeight / (apexTime * apexTime);
+        initialJumpSpeed = 2 * apexHeight / apexTime;
     }
 
     public void Update()
     {
-        
+        CheckForGround();
+
+
 
         Vector2 playerInput = new Vector2();
 
 
         playerInput.x = (Input.GetAxisRaw("Horizontal"));
+
+
+        if (isDead == true)
+        {
+            currentState = PlayerState.dead;
+
+        }
+        switch (currentState)
+        {
+            case PlayerState.dead: //do nothing
+                break;
+            case PlayerState.idle:
+                if(!Isgrounded) currentState = PlayerState.jumping;
+                else if (velocity.x != 0) currentState = PlayerState.walking;
+                break;
+            case PlayerState.walking:
+                if (!Isgrounded) currentState = PlayerState.jumping;
+                else if (velocity.x == 0) currentState = PlayerState.idle;
+                break;
+            case PlayerState.jumping:
+                if (Isgrounded)
+                {
+                    if (!Isgrounded) currentState = PlayerState.jumping;
+                    else if (velocity.x == 0) currentState = PlayerState.idle;
+                }
+                
+                break;
+        }
+
+        //if (currentState == PlayerState.dead) return;
+
+
+
         MovementUpdate(playerInput);
+        JumpUpdate();
         body.velocity = velocity;
         
-
+        if (!Isgrounded)
+        {
+            velocity.y += gravity * Time.deltaTime;
+        }
+        if (Isgrounded)
+        {
+            velocity.y = 0;
+        }
     }
 
     private void MovementUpdate(Vector2 playerInput)
@@ -84,6 +150,31 @@ public class PlayerControl2 : MonoBehaviour
 
     }
 
+    private void JumpUpdate()
+    {
+        if (Isgrounded && Input.GetButton("Jump"))
+        {
+            velocity.y = initialJumpSpeed;
+            Isgrounded = false;
+        }
+    }
+
+
+    private void CheckForGround()
+    {
+        Isgrounded = Physics2D.OverlapBox(transform.position + Vector3.down * groundCheckOffset, groundCheckSize, 0, groundCheckMask);
+
+    }
+
+    private void debugDrawGroundCheck()
+    {
+        Vector3 p1 = transform.position + Vector3.down * groundCheckOffset + new Vector3(groundCheckSize.x / 2, groundCheckSize.y / 2);
+    }
+    public void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position + Vector3.down * groundCheckOffset, groundCheckSize);
+    }
+
     public bool IsWalking()
     {
         return velocity.x != 0;
@@ -92,7 +183,7 @@ public class PlayerControl2 : MonoBehaviour
     }
     public bool IsGrounded()
     {
-        return false;
+        return !Isgrounded;
 
     }
 
--- Assets/Scripts/PlayerVis2.cs
+++ Assets/Scripts/PlayerVis2.cs
@@ -5,14 +5,16 @@ public class PlayerVis2 : MonoBehaviour
     public Animator animator;
     public SpriteRenderer bodyRenderer;
     public PlayerControl2 playerController;
-
-    private readonly int isWalkingHash = Animator.StringToHash("IsWalking");
-    private readonly int isGroundedHash = Animator.StringToHash("IsGrounded");
-
+    private readonly int IdleHash = Animator.StringToHash("Idle");
+    private readonly int WalkingHash = Animator.StringToHash("Walking");
+    private readonly int JumpingHash = Animator.StringToHash("Jumping");
+    private readonly int DeadHash = Animator.StringToHash("Dead");
     void Update()
     {
-        animator.SetBool(isWalkingHash, playerController.IsWalking());
-        animator.SetBool(isGroundedHash, playerController.IsGrounded());
+        //UpdateVisuals();
+
+        animator.SetBool(WalkingHash, playerController.IsWalking());
+        animator.SetBool(JumpingHash, playerController.IsGrounded());
 
         switch (playerController.GetFacingDirection())
         {
@@ -24,4 +26,31 @@ public class PlayerVis2 : MonoBehaviour
                 break;
         }
     }
+
+    private void UpdateVisuals()
+    {
+        if (playerController.previousState != playerController.currentState)
+        {
+            switch (playerController.currentState)
+            {
+                case PlayerControl2.PlayerState.idle:
+                    animator.CrossFade(IdleHash, 0);
+                    break;
+                case PlayerControl2.PlayerState.walking:
+                    animator.CrossFade(WalkingHash, 0);
+                    break;
+                case PlayerControl2.PlayerState.jumping:
+                    animator.CrossFade(JumpingHash, 0);
+                    break;
+                case PlayerControl2.PlayerState.dead:
+                    animator.CrossFade(DeadHash, 0);
+                    break;
+            }
+
+
+        }
+    }
+
+
+
 }
